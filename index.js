9427647bb7878e5265786e2e1c7644fdb946e19c
pizzaandpast4 <dmitrij.fiodorov@gmail.com>
2024-07-21 22:40:46 +0300

    aaa

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-console.log('demo.....')
-console.log('demo.....')
-console.log('demo.....')
+import { Dog } from "./js/Dog.js";
+
+const rexas = new Dog('Rexas', 'black', 3);
+console.log(rexas);
+console.log(rexas.hi());
+console.log(rexas.think());
+console.log(rexas.addNumbers(1, 3));
+console.log(rexas.addNumbers(-1, 3));
+console.log(rexas.addNumbers(-1, -3));
+console.log(rexas.addNumbers(-1, 1));
+console.log(rexas.manyBones(10));
+
+console.clear();
+
+const brisius = new Dog('Brisius', 'white', 2);
+console.log(brisius);
+
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+
+console.log(brisius.birthday());
+console.log(brisius.birthday());
+console.log(brisius.birthday());
+console.log(brisius.birthday());
---
+++ js/Dog.js
@@ -0,0 +1,48 @@
+export class Dog {
+    constructor(vardas, kailioSpalva, amzius) {
+        this.name = vardas;
+        this.furColor = kailioSpalva;
+        this.age = amzius;
+        this.legsCount = 4;
+        this.hasTail = true;
+    }
+
+    birthday() {
+        this.age++;
+        return `Suns ${this.age} gimtadienis! 🥳`;
+    }
+
+    lostLeg() {
+        if (this.legsCount === 0) {
+            return `Suo jau turi 0 (nuli) koju... daugiau negalima prarasti`;
+        }
+        this.legsCount--;
+        return `Suo prarado koja... 😢`;
+    }
+
+    hi() {
+        return 'Suo sako: au au au!!';
+    }
+
+    think() {
+        return 'Suo galvoja: 🦴🦴🦴'
+    }
+
+    addNumbers(a, b) {
+        if (a + b < 0) {
+            return 'Suo yra liudnas, negali suskaiciuoti negatyvaus kaulu kiekio :(';
+        }
+
+        if (a + b === 0) {
+            return 'Suns reakcija 👀';
+        }
+
+        return `Suo suskaiciavo ${a} + ${b} = ${'🦴'.repeat(a + b)}.`;
+    }
+
+    manyBones(count) {
+        return `Many bones: ${'🦴'.repeat(count)}.`;
+    }
+
+}
+
