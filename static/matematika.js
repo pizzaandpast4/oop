394d007a1155a2f65b60d5d6a0a828f5d746c600
pizzaandpast4 <dmitrij.fiodorov@gmail.com>
2024-07-24 12:37:19 +0300

    matematika

---
+++ static/matematika.js
@@ -0,0 +1,13 @@
+export const matematika = {
+    add: (a, b) => a + b,
+    diff: (a, b) => a - b,
+    multi: (a, b) => a * b,
+    div: (a, b) => a / b,
+    mod: (a, b) => a % b,
+};
+
+console.log(matematika.add(11, 19));
+console.log(matematika.diff(11, 19));
+console.log(matematika.multi(11, 19));
+console.log(matematika.div(11, 19));
+console.log(matematika.mod(11, 19));
